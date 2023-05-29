9dda977b9e9ca56539e8d9f517ff2e3f7a32bd4c
ribardej <ribardej@student.cvut.cz>
2023-05-29 12:52:58 +0200

    basic interactiveness implemented

--- index.js
+++ index.js
@@ -1,5 +1,98 @@
-document.getElementById('excelFile').addEventListener('change', handleFile, false);
+class EditTable {
+    #table
+    #tableHead
+    #tableBody
+    #buttonLeft
+    #buttonBottom
+
+    constructor(){
+        const tableContainer = document.getElementById('tableContainer');
+        tableContainer.innerHTML = '';
+        
+        this.#table = document.createElement('table');
+        this.#table.id = 'excelDataTable';
+        this.#tableHead = document.createElement('thead');
+        this.#tableBody = document.createElement('tbody');
+
+        this.#table.appendChild(this.#tableHead);
+        this.#table.appendChild(this.#tableBody);
+        tableContainer.appendChild(this.#table);
+    }
+
+    renderTable(data) {
+        this.#tableHead.innerHTML = '';
+        this.#tableBody.innerHTML = '';
+
+        const headerRow = document.createElement('tr');
+        data[0].forEach(function(cellData) {
+            const headerCell = document.createElement('th');
+            headerCell.textContent = cellData;
+            headerRow.appendChild(headerCell);
+        });
+        
+        this.#tableHead.appendChild(headerRow);
+        
+        for (let i = 1; i < data.length; i++) {
+            const row = document.createElement('tr');
+        
+            data[i].forEach(function(cellData) {
+            const cell = document.createElement('td');
+            cell.textContent = cellData;
+            row.appendChild(cell);
+            });
+        
+            this.#tableBody.appendChild(row);
+        }
+        
+        let cells = this.#table.getElementsByTagName('td');
+        for (let i = 0; i < cells.length; i++) {
+            cells[i].addEventListener('click', editCell);
+        }
+        let headers = this.#table.getElementsByTagName('th');
+        for (let i = 0; i < headers.length; i++) {
+            headers[i].addEventListener('click', editCell);
+        }
+    } 
+}
+
+function exportTableToExcel(tableId, filename) {
+    const table = document.getElementById(tableId);
+    if(table === null) {
+        alert('Table not found');
+        return;
+    }
+    if(table.nodeName !== 'TABLE' || table.nodeName === null) {
+        alert('Invalid table')
+        return;
+    }
+    if(table.rows.length === 0) {
+        alert('Table is empty');
+        return;
+    }
+    const workbook = XLSX.utils.table_to_book(table, { sheet: 'Sheet 1' });
+    const excelBuffer = XLSX.write(workbook, { bookType: 'xlsx', type: 'array' });
+    const blob = new Blob([excelBuffer], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' });
   
+    if (navigator.msSaveBlob) { // For IE
+      navigator.msSaveBlob(blob, filename);
+    } else {
+      const link = document.createElement('a');
+      if (link.download !== undefined) { // Other browsers
+        const url = URL.createObjectURL(blob);
+        link.setAttribute('href', url);
+        link.setAttribute('download', filename);
+        link.style.visibility = 'hidden';
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+      }
+    }
+  }
+
+document.getElementById('excelFile').addEventListener('change', handleFile, false);
+
+const editTable = new EditTable();
+
 function handleFile(event) {
 const file = event.target.files[0];
 const reader = new FileReader();
@@ -10,42 +103,40 @@ reader.onload = function(e) {
     const worksheet = workbook.Sheets[workbook.SheetNames[0]];
     const jsonData = XLSX.utils.sheet_to_json(worksheet, { header: 1 });
 
-    renderTable(jsonData);
+    editTable.renderTable(jsonData);
 };
 
 reader.readAsArrayBuffer(file);
 }
 
-function renderTable(data) {
-const tableContainer = document.getElementById('tableContainer');
-tableContainer.innerHTML = '';
+document.getElementById('buttonDownload').addEventListener('click', exportTableToExcel.bind(null, 'excelDataTable', 'excelData.xlsx'));
 
-const table = document.createElement('table');
-const tableHead = document.createElement('thead');
-const tableBody = document.createElement('tbody');
+function editCell() {
+    if(this.innerHTML === '<input type="text">') return;
+    console.log('editCell');
+    let cell = this;
+    const oldValue = cell.innerHTML;
 
-const headerRow = document.createElement('tr');
-data[0].forEach(function(cellData) {
-    const headerCell = document.createElement('th');
-    headerCell.textContent = cellData;
-    headerRow.appendChild(headerCell);
-});
+    // Create an input element
+    let input = document.createElement('input');
+    input.type = 'text';
+    input.value = oldValue;
 
-tableHead.appendChild(headerRow);
+    // Replace the cell content with the input element
+    cell.innerHTML = '';
+    cell.appendChild(input);
 
-for (let i = 1; i < data.length; i++) {
-    const row = document.createElement('tr');
+    // Focus on the input element
+    input.focus();
 
-    data[i].forEach(function(cellData) {
-    const cell = document.createElement('td');
-    cell.textContent = cellData;
-    row.appendChild(cell);
+    // Add event listener to handle input change
+    input.addEventListener('blur', function () {
+        let newValue = input.value;
+        console.log(newValue);
+        cell.innerHTML = newValue;
     });
 
-    tableBody.appendChild(row);
-}
-
-table.appendChild(tableHead);
-table.appendChild(tableBody);
-tableContainer.appendChild(table);
+    input.addEventListener('keypress', function (event) {
+        if(event.key === "Enter") input.blur();
+    });
 }
